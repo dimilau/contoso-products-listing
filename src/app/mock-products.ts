f1f7561a729138080d8aea9975e27d4a441a8fbb
Low Sum Wai <lowsumwai@gmail.com>
2020-11-25 13:09:46 +0800

    Show list in app component

---
+++ src/app/mock-products.ts
@@ -0,0 +1,10 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+    { id: 1, name: 'Kone', price: 3488.99 },
+    { id: 2, name: 'Ironhide', price: 3299.99 },
+    { id: 3, name: 'Ironhide Cartridge', price: 529.99 },
+    { id: 4, name: 'Fox + Float', price: 66 },
+    { id: 5, name: 'Shimano + Derailleur', price: 67.60},
+    { id: 6, name: 'SANTA CRUZ', price: 185.50}
+];
