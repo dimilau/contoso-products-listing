a767c8246e0a15fdd5b9aa9e16a16815256584b4
Low Sum Wai <lowsumwai@gmail.com>
2020-11-25 15:03:21 +0800

    List product and product details

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PRODUCTS } from '../mock-products';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
+  getProduct(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.product = PRODUCTS[id];
+  }
+
+}
